7a17eeb0f6f53c708dd92a403a23d45571080b5c
Julian Pinto <juliapinto700@gmail.com>
2025-01-29 17:52:09 -0500

    Se sube devueltaa x2

--- app/models/viewsModel.php
+++ app/models/viewsModel.php
@@ -7,11 +7,15 @@
 		/*---------- Modelo obtener vista ----------*/
 		protected function obtenerVistasModelo($vista){
 
+<<<<<<< HEAD
 <<<<<<< Updated upstream
 			$listaBlanca=["dashboard","userNew","userList","cliente","autor","inventario","categoria","proveedor","editorial","userUpdate","userSearch","userPhoto","logOut"];
 =======
 			$listaBlanca=["dashboard","userNew","userList","cliente","autor","inventario","categoria", "subcategoria", "proveedor","vender","editorial","userUpdate","userSearch","userPhoto","logOut"];
 >>>>>>> Stashed changes
+=======
+			$listaBlanca=["dashboard","userNew","userList","cliente","autor","inventario","categoria","proveedor","editorial","userUpdate","userSearch","userPhoto","logOut"];
+>>>>>>> parent of 484005b (Se suben cambios de diseño y relaciones)
 
 			if(in_array($vista, $listaBlanca)){
 				if(is_file("./app/views/content/".$vista."-view.php")){
